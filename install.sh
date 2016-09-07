0439bac52d3f8c58185cd7879011449f728fd76c
Peter Fern <github@obfusc8.org>
2016-09-07 11:19:20 +1000

    Install script no modifies default objectClasses and existing users

--- install.sh
+++ install.sh
@@ -18,6 +18,31 @@ else
 	PYPATH="/usr/lib/python2.*/dist-packages/ipalib/plugins"
 fi
 
+# Install files
 sudo mkdir -p "${JSPATH}"
 sudo cp -v $(dirname $0)/user_mailalternateaddress.js "${JSPATH}/"
 sudo cp -v $(dirname $0)/user_mailalternateaddress.py ${PYPATH}/
+
+# Update default user objectClasses
+kinit admin
+objectClasses="$(ipa config-show --raw --all |grep ipaUserObjectClasses |awk '{print "--userobjectclasses="$2}' |tr '\n' ' ')"
+echo "$objectClasses" | grep -i mailrecipient || ipa config-mod $objectClasses --userobjectclasses=mailrecipient
+
+# Add objectClass to existing users
+updateUsers() {
+	baseDn="$(ipa config-show --raw --all |grep -e '^[[:space:]]*dn:' |sed -e 's#^[[:space:]]*dn: cn=ipaConfig,cn=etc,##g')"
+	ldapsearch -Y GSSAPI -b "cn=users,cn=accounts,${baseDn}" -s one -LLL dn | while read line ; do
+		if [ -n "$line" ] ; then
+			echo -e "${line}\nchangetype: modify\nadd: objectclass\nobjectclass: mailRecipient\n\n" | ldapmodify -Y GSSAPI
+		fi
+	done
+}
+
+echo
+read -n 1 -r -p "Enable aliases for existing users? [Y/n] " REPLY
+[ -z "$REPLY" ] && REPLY=Y
+case $REPLY in
+	[yY]) updateUsers ;;
+esac
+
+sudo systemctl restart ipa
